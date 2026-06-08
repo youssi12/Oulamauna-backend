ba6470940a22285443f86a4376a7f324495181a9
youssi12 <y.oulahcene@esi-sba.com>
2026-06-08 19:56:43 +0100

    setup

--- config/db.js
+++ config/db.js
@@ -1,17 +1,7 @@
-const mysql2 = require("mysql2");
- 
+ const { PrismaClient } = require("@prisma/client");
+
 require("dotenv").config();
 
-const  pool = mysql2.createPool({
-host:process.env.DB_HOST,
-user:process.env.DB_USER,
-password:process.env.DB_PASSWORD,
-database:process.env.DB_NAME,
-waitForConnections: true,
-connectionLimit: 10,
-queueLimit: 0
-})
+const prisma = new PrismaClient();
 
- 
-
-module.exports = pool.promise();
+module.exports = prisma;
